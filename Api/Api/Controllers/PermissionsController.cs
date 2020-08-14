e1d57098bce8aa610a215af6ccc52e280b584c69
Héctor Rosario <36975677+hectorrosario22@users.noreply.github.com>
2020-08-14 08:47:25 -0400

    add endpoint to get all permissions

---
+++ Api/Api/Controllers/PermissionsController.cs
@@ -0,0 +1,27 @@
+﻿using Api.Application.UseCases.Permissions.Dtos;
+using Api.Application.UseCases.Permissions.Queries.GetAllPermissions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PermissionsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public PermissionsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task<IEnumerable<PermissionDto>> GetAll()
+        {
+            var permissions = await _mediator.Send(new GetAllPermissionsQuery());
+            return permissions;
+        }
+    }
+}
