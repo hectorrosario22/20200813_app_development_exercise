a29b1a9853cd8ae7a42db9092a0a61ea42f6a854
Héctor Rosario <36975677+hectorrosario22@users.noreply.github.com>
2020-08-13 22:36:14 -0400

    add endpoint to get all permission types

---
+++ Api/Api/Controllers/PermissionTypesController.cs
@@ -0,0 +1,26 @@
+﻿using Api.Application.UseCases.PermissionTypes.Queries.GetAllPermissionTypes;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PermissionTypesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public PermissionTypesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task<IEnumerable<PermissionTypeDto>> GetAll()
+        {
+            var permissionTypeDtos = await _mediator.Send(new GetAllPermissionTypesRequest());
+            return permissionTypeDtos;
+        }
+    }
+}
