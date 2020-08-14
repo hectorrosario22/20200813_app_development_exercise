ca41f86f492553bf25ec88ac6ed3e45ef2efd08f
Héctor Rosario <36975677+hectorrosario22@users.noreply.github.com>
2020-08-13 21:41:15 -0400

    add entities

---
+++ Api/Api.Domain/Entities/Permission.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Api.Domain.Entities
+{
+    public class Permission
+    {
+        public int Id { get; set; }
+        public string EmployeeFirstName { get; set; }
+        public string EmployeeLastName { get; set; }
+        public short PermissionTypeId { get; set; }
+        public DateTimeOffset PermissionDate { get; private set; } = DateTimeOffset.UtcNow;
+
+        public PermissionType PermissionType { get; set; }
+    }
+}
---
+++ Api/Api.Domain/Entities/PermissionType.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Api.Domain.Entities
+{
+    public class PermissionType
+    {
+        public short Id { get; set; }
+        public string Description { get; set; }
+
+        public ICollection<Permission> Permissions { get; private set; } = new List<Permission>();
+    }
+}
