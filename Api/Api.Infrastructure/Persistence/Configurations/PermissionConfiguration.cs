3724b5704c110f63152988e847ed983e61655c54
Héctor Rosario <36975677+hectorrosario22@users.noreply.github.com>
2020-08-13 22:20:44 -0400

    add entities configuration

---
+++ Api/Api.Infrastructure/Persistence/Configurations/PermissionConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Api.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Api.Infrastructure.Persistence.Configurations
+{
+    public class PermissionConfiguration : IEntityTypeConfiguration<Permission>
+    {
+        public void Configure(EntityTypeBuilder<Permission> builder)
+        {
+            builder.HasKey(d => d.Id);
+
+            builder.Property(d => d.EmployeeFirstName).IsRequired().HasMaxLength(100);
+            builder.Property(d => d.EmployeeLastName).IsRequired().HasMaxLength(100);
+            builder.Property(d => d.PermissionTypeId).IsRequired();
+            builder.Property(d => d.PermissionDate).IsRequired();
+        }
+    }
+}
---
+++ Api/Api.Infrastructure/Persistence/Configurations/PermissionTypeConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Api.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Api.Infrastructure.Persistence.Configurations
+{
+    public class PermissionTypeConfiguration : IEntityTypeConfiguration<PermissionType>
+    {
+        public void Configure(EntityTypeBuilder<PermissionType> builder)
+        {
+            builder.HasKey(d => d.Id);
+
+            builder.Property(d => d.Description).IsRequired().HasMaxLength(100);
+            builder.HasMany(d => d.Permissions).WithOne(d => d.PermissionType).HasForeignKey(d => d.PermissionTypeId);
+        }
+    }
+}
