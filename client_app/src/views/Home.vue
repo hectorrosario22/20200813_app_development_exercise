7c1571e632cd7e3c6f4398c5ed025b32a0688f5f
Héctor Rosario <36975677+hectorrosario22@users.noreply.github.com>
2020-08-14 10:38:08 -0400

    show permissions

--- client_app/src/views/Home.vue
+++ client_app/src/views/Home.vue
@@ -1,18 +1,54 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th scope="col">Id</th>
+          <th scope="col">Nombre Empleado</th>
+          <th scope="col">Apellido Empleado</th>
+          <th scope="col">Tipo Permiso</th>
+          <th scope="col">Fecha Permiso</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(permission, index) in permissions" :key="index">
+          <th scope="row" v-text="permission.id">1</th>
+          <td v-text="permission.employeeFirstName"></td>
+          <td v-text="permission.employeeLastName"></td>
+          <td v-text="permission.permissionTypeDescription"></td>
+          <td>{{ permission.permissionDate | formatDate }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import moment from "moment";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  data() {
+    return {
+      permissions: [],
+    };
+  },
+  methods: {
+    async getAllPermissions() {
+      const url = `${process.env.VUE_APP_API_URL}/permissions`;
+      let result = await fetch(url, {
+        method: "GET",
+      });
+      this.permissions = await result.json();
+    },
+  },
+  created() {
+    this.getAllPermissions();
+  },
+  filters: {
+    formatDate(date) {
+      return moment(date).lang("es-do").format("DD [de] MMMM [del] YYYY");
+    },
+  },
+};
 </script>
