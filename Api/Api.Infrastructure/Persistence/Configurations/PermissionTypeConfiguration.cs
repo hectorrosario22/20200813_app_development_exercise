42c8228c9cb58e5b68217588e583015379bed32c
Héctor Rosario <36975677+hectorrosario22@users.noreply.github.com>
2020-08-14 08:02:22 -0400

    add default permission types

--- Api/Api.Infrastructure/Persistence/Configurations/PermissionTypeConfiguration.cs
+++ Api/Api.Infrastructure/Persistence/Configurations/PermissionTypeConfiguration.cs
@@ -12,6 +12,50 @@ namespace Api.Infrastructure.Persistence.Configurations
 
             builder.Property(d => d.Description).IsRequired().HasMaxLength(100);
             builder.HasMany(d => d.Permissions).WithOne(d => d.PermissionType).HasForeignKey(d => d.PermissionTypeId);
+
+            builder.HasData(new PermissionType[]
+            {
+                new PermissionType
+                {
+                    Id = 1,
+                    Description = "Matrimonio"
+                },
+                new PermissionType
+                {
+                    Id = 2,
+                    Description = "Nacimiento de un hijo"
+                },
+                new PermissionType
+                {
+                    Id = 3,
+                    Description = "Fallecimiento, accidente o enfermedad grave de un familiar"
+                },
+                new PermissionType
+                {
+                    Id = 4,
+                    Description = "Mudanza"
+                },
+                new PermissionType
+                {
+                    Id = 5,
+                    Description = "Deberes públicos"
+                },
+                new PermissionType
+                {
+                    Id = 6,
+                    Description = "Función sindical o representación de trabajadores"
+                },
+                new PermissionType
+                {
+                    Id = 7,
+                    Description = "Exámenes prenatales o para preparar el parto"
+                },
+                new PermissionType
+                {
+                    Id = 8,
+                    Description = "Exámenes"
+                }
+            });
         }
     }
 }
---
+++ Api/Api/Migrations/20200814120043_AddDefaultPermissionTypes.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Api.Migrations
+{
+    public partial class AddDefaultPermissionTypes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "PermissionTypes",
+                columns: new[] { "Id", "Description" },
+                values: new object[,]
+                {
+                    { (short)1, "Matrimonio" },
+                    { (short)2, "Nacimiento de un hijo" },
+                    { (short)3, "Fallecimiento, accidente o enfermedad grave de un familiar" },
+                    { (short)4, "Mudanza" },
+                    { (short)5, "Deberes públicos" },
+                    { (short)6, "Función sindical o representación de trabajadores" },
+                    { (short)7, "Exámenes prenatales o para preparar el parto" },
+                    { (short)8, "Exámenes" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "PermissionTypes",
+                keyColumn: "Id",
+                keyValue: (short)1);
+
+            migrationBuilder.DeleteData(
+                table: "PermissionTypes",
+                keyColumn: "Id",
+                keyValue: (short)2);
+
+            migrationBuilder.DeleteData(
+                table: "PermissionTypes",
+                keyColumn: "Id",
+                keyValue: (short)3);
+
+            migrationBuilder.DeleteData(
+                table: "PermissionTypes",
+                keyColumn: "Id",
+                keyValue: (short)4);
+
+            migrationBuilder.DeleteData(
+                table: "PermissionTypes",
+                keyColumn: "Id",
+                keyValue: (short)5);
+
+            migrationBuilder.DeleteData(
+                table: "PermissionTypes",
+                keyColumn: "Id",
+                keyValue: (short)6);
+
+            migrationBuilder.DeleteData(
+                table: "PermissionTypes",
+                keyColumn: "Id",
+                keyValue: (short)7);
+
+            migrationBuilder.DeleteData(
+                table: "PermissionTypes",
+                keyColumn: "Id",
+                keyValue: (short)8);
+        }
+    }
+}
